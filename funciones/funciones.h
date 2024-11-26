e408dbb96c5e8020205549e6a83a1551490e19fe
Maaaikol <mreachu@myuax.com>
2024-11-26 10:58:00 +0100

    gg

---
+++ funciones/funciones.h
@@ -0,0 +1,10 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+
+
+void load_script(const char* filename, bool show_script = false);
+void load_script();
+
+
+#endif //FUNCIONES_H
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "Funciones/ejecutar.h"
+
+int main() {
+    load_script();
+    return 0;
+}
